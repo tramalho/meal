d100e835c625ea261f9751ccfb0863cab417e551
trama <thiagofacul@gmail.com>
2019-01-20 19:49:43 -0200

    fix glide imports

---
+++ app/src/main/java/br/com/tramalho/meal/utilities/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package br.com.tramalho.meal.utilities
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
